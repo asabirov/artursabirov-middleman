03b0ec343aca598f1dc9fb4c1014f69ccc7a0b39
a.sabirov <a.sabirov@fun-box.ru>
2014-04-02 08:26:06 +0400

    add sumodule build

--- config.rb
+++ config.rb
@@ -9,9 +9,12 @@ activate :blog do |blog|
   blog.prefix = "dev"
   blog.permalink = "{year}/{month}/{day}/{alias}.html"
   blog.layout = "article"
-  # Matcher for blog source files
+  blog.tag_template = "tag.html"
+
+  blog.paginate = true
+
   # blog.sources = "{year}-{month}-{day}-{title}.html"
-  #blog.taglink = "tags/{tag}.html"
+  # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
@@ -19,14 +22,7 @@ activate :blog do |blog|
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
   #blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  blog.paginate = true
-  #blog.per_page = 5
-  #blog.page_link = "/dev/page/{num}"
 end
 
 activate :blog do |blog|
@@ -43,33 +39,9 @@ end
 
 page "/feed.xml", layout: false
 
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
+##
 # Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-
-#with_layout :article do
-#   page "/dev/*"
-#end
+##
 
 with_layout :tag do
   page "/dev/tags/*"
@@ -84,24 +56,12 @@ end
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-###
+##
 # Helpers
-###
+##
 
 activate :syntax
 
-set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true
-
-activate :autoprefixer
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
 helpers do
   def blog_title
     "Артур Сабиров"
@@ -156,26 +116,32 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+###
+# Extensions
+###
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :autoprefixer
 activate :directory_indexes
+# activate :automatic_image_sizes
+# activate :livereload
 
-# Build-specific configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
 
-  # Enable cache buster
   # activate :asset_hash
-
-  # Use relative URLs
   # activate :relative_assets
-
-  # Or use a different image path
   # set :http_prefix, "/Content/images/"
 
-  set :build_dir, "public"
+  set :build_dir, "build"
 
   ignore "article.html.haml"
   ignore "page.html.haml"
