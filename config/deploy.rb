c114461469349e5f40ee5aa197d89c3d3e8a5a6e
a.sabirov <a.sabirov@fun-box.ru>
2014-04-02 08:14:20 +0400

    update build repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,12 @@
 lock '3.1.0'
 
 set :application, 'ArturSabirov.ru'
-set :repo_url, 'git@bitbucket.org:asabirov/artursabirov.git'
-set :user, 'delingcity'
+set :repo_url, 'git@github.com:asabirov/artursabirov-build.git'
 set :log_level, :debug
 set :keep_releases, 3
 
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-1.9.3-p484'
-
 
 task :build do
   system "middleman build"
